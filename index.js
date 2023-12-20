7e772e0f41271ff6bd6ffb732bfb2af9e91b8d4d
Eric Maranga <ekirira22@gmail.com>
2023-12-20 13:36:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+//Test 1. Returns an array of thank you messages for each name provided to the function
+
+function writeCards(arrStr, event){
+    const thankYouMsgs = []
+    for(let i =0; i<arrStr.length; i++){
+        let outputMesg = `Thank you, ${arrStr[i]}, for the wonderful ${event} gift!`
+        thankYouMsgs.push(outputMesg)
+
+        // console.log(thankYouMsgs)
+        // debugger
+    }
+
+    return thankYouMsgs
+}
+
+//Test 2. Invokes console.log once for each number, counting down from the number provided to zero
+
+function countDown(num){
+    while(typeof num === 'number' && num >= 0){
+        console.log(num--)
+    }
+}
+
